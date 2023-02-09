3ab1af8556c785d156452edfe2195b6342b684c0
lunar_commands <tfeabp@gmail.com>
2023-02-09 15:04:27 +0100

    Mess with Svelte.

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,7 +1,14 @@
 <script>
-  export let post = [];
+  export let post;
 </script>
 
-{#each post as image}
-  <img src={image.src} alt={image.alt} />
-{/each}
+{console.log(post.data)}
+<ul>
+  {#each post.data as { title, description, images }}
+    <li>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <img src={images.src} alt={images.alt} />
+    </li>
+  {/each}
+</ul>
