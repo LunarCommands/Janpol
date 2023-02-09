d84b086a708a28614e4c0e865066c2e6453f8d45
lunar_commands <tfeabp@gmail.com>
2023-02-09 18:20:16 +0100

    Working Gallery.svelte component.

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -2,13 +2,6 @@
   export let post;
 </script>
 
-{console.log(post.data)}
-<ul>
-  {#each post.data as { title, description, images }}
-    <li>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <img src={images.src} alt={images.alt} />
-    </li>
-  {/each}
-</ul>
+{#each post.data.images as p}
+  <img src={p.src} alt={p.alt} />
+{/each}
