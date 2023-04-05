f914357af9154206f3d5d7cb682f62cbfce6a6e3
lunar_commands <tfeabp@gmail.com>
2023-04-05 18:18:28 +0200

    Revert "Failing revert."
    This reverts commit 7966f05ffb5d9a7f5a30ecf6e9192ef239f06d70.

---
+++ src/components/P.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let text;
+</script>
+
+<p class="my-8 text-lg font-normal text-gray-300 lg:text-xl px-10">
+  {text}
+</p>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,15 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				sans: ['Inter', 'Helvetica', 'Arial', 'sans-serif']
-			}
+			},
+			h1Custom: {
+				margin: '0 10px',
+				fontSize: '4rem',
+				fontWeight: 'extrabold',
+				lineHeight: '1',
+				letterSpacing: '-0.05em',
+				color: '#D1D5DB',
+    },
 		},
 	},
 	plugins: [
