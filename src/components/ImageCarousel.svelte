3e2990ed8083974a9efecbc1d6d9658886fbde78
lunar_commands <tfeabp@gmail.com>
2023-04-08 12:29:36 +0200

    Adding a trailing dot to the ImageCarousel.svelte.

--- src/components/ImageCarousel.svelte
+++ src/components/ImageCarousel.svelte
@@ -33,13 +33,25 @@
   `}
       />
     {/each}
+    <div
+      class="absolute bottom-0 left-0 right-0 flex justify-center space-x-2 mb-4"
+    >
+      {#each images as _, index}
+        <button
+          class="h-3 w-3 rounded-full bg-gray-300 transition-colors duration-200 ease-out
+            hover:bg-gray-400 focus:bg-gray-400 focus:outline-none
+            {currentIndex === index ? 'bg-gray-700' : ''}"
+          on:click={() => (currentIndex = index)}
+        />
+      {/each}
+    </div>
   </div>
   <button
     class="bg-gray-500 text-white rounded-full w-10 h-10 flex items-center justify-center absolute top-50 transform translate(-50%, -50%)"
     style="left: 25%"
     on:click={handlePrev}
   >
-    <svg class="w-4 h-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+    <svg class="w-5 h-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M7.05 9.293l-.707.707L12 15.657l1.414-1.414L8.636 10l3.535-3.536-1.414-1.414z"
       />
@@ -50,11 +62,7 @@
     style="right: 25%"
     on:click={handleNext}
   >
-    <svg
-      class=" w-4 h-4"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+    <svg class="w-5 h-5" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"
       />
