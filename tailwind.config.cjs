816cbc882ffd0fec0e59d1717cd42eb94ebcfce5
lunar_commands <tfeabp@gmail.com>
2023-04-08 19:32:00 +0200

    Second attempt to deploy the website.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,8 @@
 module.exports = {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'],
+		'./node_modules/flowbite/**/*.js',
+		"./dist/_astro/*.*",],
 	theme: {
 		extend: {
 			fontFamily: {
