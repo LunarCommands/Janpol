a626e9d4ac5562cdf26f3c4b8a17a1bdeaf92238
lunar_commands <tfeabp@gmail.com>
2023-04-10 11:50:40 +0200

    Fixing the ImageCarousel component which was giving the wrong image path.

--- src/components/ImageCarousel.svelte
+++ src/components/ImageCarousel.svelte
@@ -29,7 +29,7 @@
     {#each images as image, index}
       <img
         key={index}
-        src={"/" + image.src}
+        src={image.src}
         alt={image.alt}
         style={`
           position: absolute;
